c8ddb471b14ff46a36a5d7d200a6d8d5bbe6b51d
ALEN TL <alen@pop-os.localdomain>
2021-09-10 15:35:47 +0530

    Major Update on the way

---
+++ fivem_resources/resources/loadingscreen/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+author 'ALEN TL'
+description 'Fivem loading screen for KLRP server'
+
+files {
+    'index.html',
+    'keks.css',
+    'centurygothic.ttf',
+    'backup.jpg',
+    'config.js'
+}
+
+loadscreen 'index.html'
